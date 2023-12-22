de06a8d1331a09db05f3a8c2ba6cb00ad7e73bd2
Max Collins <111886291+TheMustardJarz@users.noreply.github.com>
2023-12-21 20:03:22 -0500

    Created SQL test file

---
+++ server/api/seratoth_api.py
@@ -0,0 +1,10 @@
+from flask_restful import Resource
+
+from flask_restful import request
+from flask_restful import reqparse
+import json
+from .db_utils import *
+
+class Front_Page(Resource):
+    def get(self):
+        return exec_get_one("SELECT numVal FROM test WHERE id=1")
--- server/server.py
+++ server/server.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
+from api.db_utils import *
+from api.seratoth_api import *
+
 app = Flask(__name__) #create Flask instance
 CORS(app) #Enable CORS on Flask server to work with Nodejs pages
 api = Api(app) #api router
+
+api.add_resource(Front_Page,'/')
 
 if __name__ == '__main__':
     print("Loading db");
